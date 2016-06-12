a4be5ba3b35c6a2f107752a66ba0e0aed94c0913
YidanGan <YidanGan@users.noreply.github.com>
2016-06-12 12:55:45 -0500

    Update UniqueBinarySearchTreeII.java

--- src/dp/UniqueBinarySearchTreeII.java
+++ src/dp/UniqueBinarySearchTreeII.java
@@ -83,3 +83,19 @@ class Solution2 {
         return node;
     }
 }
+
+//unique binary search trees, return the number of trees
+class Solution0{
+    public int numTrees(int n) {
+        int[] res = new int[n+1];
+        res[0] = 1;
+        res[1] = 1;
+        
+        for(int i = 2; i <= n; i++) {
+            for(int j = 1; j <=i; j++) {
+                res[i] +=res[j-1]*res[i-j];
+            }
+        }
+        return res[n];
+    }
+}
