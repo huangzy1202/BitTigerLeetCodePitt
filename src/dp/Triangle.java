28d494d4ef437de0b0b52e95773c3417259b11ad
YidanGan <YidanGan@users.noreply.github.com>
2016-06-12 12:32:17 -0500

    Create Triangle.java

---
+++ src/dp/Triangle.java
@@ -0,0 +1,13 @@
+//https://leetcode.com/discuss/53694/7-lines-neat-java-solution
+public class Solution {
+    public int minimumTotal(List<List<Integer>> triangle) {
+        int[] A = new int[triangle.size()+1];
+    
+        for(int i=triangle.size()-1;i>=0;i--){
+            for(int j=0;j<triangle.get(i).size();j++){
+                A[j] = Math.min(A[j],A[j+1])+triangle.get(i).get(j);
+            }
+        }
+        return A[0];
+    }
+}
