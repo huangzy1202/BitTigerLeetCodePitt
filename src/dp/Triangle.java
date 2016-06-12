9b315fd30010c7a09d89b04cb7a6c71e077675c2
YidanGan <YidanGan@users.noreply.github.com>
2016-06-12 12:37:52 -0500

    Update Triangle.java

--- src/dp/Triangle.java
+++ src/dp/Triangle.java
@@ -11,3 +11,20 @@ public class Solution {
         return A[0];
     }
 }
+
+//modify the input list, might be unsafe
+class Solution2{
+ public int minimumTotal(List<List<Integer>> triangle) {
+        if(triangle.size() == 0)
+            return 0;
+
+        for (int i=triangle.size() - 2; i>=0; i--) {
+            for (int j=0; j<=i; j++) {
+                List<Integer> nextRow = triangle.get(i+1);
+                int sum = Math.min(nextRow.get(j), nextRow.get(j+1)) + triangle.get(i).get(j);
+                triangle.get(i).set(j, sum);
+            }
+        }
+        return triangle.get(0).get(0);
+    }
+}
