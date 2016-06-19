e3f8be990c6264476512f7b43f436bd3d6791db4
YidanGan <YidanGan@users.noreply.github.com>
2016-06-19 11:45:32 -0500

    Update SubsetsII.java

--- src/backtracking/SubsetsII.java
+++ src/backtracking/SubsetsII.java
@@ -20,4 +20,28 @@ public class Solution {
             path.remove(path.size()-1);
         }
     }
+    
+    //iteration
+    public List<List<Integer>> subsetsWithDup2(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        res.add(new ArrayList<Integer>());
+        if(nums == null || nums.length == 0) return res;
+
+        Arrays.sort(nums);
+        int size = 0, start;
+        for(int i = 0; i < nums.length; i++) {
+            if(i >= 1 && nums[i] == nums[i - 1]) {
+                start = size;
+            }else {
+                start = 0;
+            }
+            size = res.size();
+            for(int j = start; j < size; j++) {
+                List<Integer> subset = new ArrayList<>(res.get(j));
+                subset.add(nums[i]);
+                res.add(subset);
+            }
+        }
+        return res;
+    }
 }
