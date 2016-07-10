ac0fb1ec12be86ba46b825fdb8006f82f4edef55
YidanGan <YidanGan@users.noreply.github.com>
2016-07-10 11:04:10 -0500

    2016-07-10

---
+++ src/LinkedList/29divide_two_integers.java
@@ -0,0 +1,38 @@
+public class Solution {
+    public int divide(int dividend, int divisor) {
+        if(divisor == 0 || dividend == Integer.MIN_VALUE && divisor == -1) 
+            return Integer.MAX_VALUE;
+            
+        int sign = 1;
+        if(dividend > 0 && divisor <0 || dividend < 0 && divisor > 0)
+            sign = -1;
+            
+        long dvd = Math.abs((long) dividend);
+        long dvs = Math.abs((long) divisor);
+        
+        int res = 0;
+        while(dvd >= dvs) {
+            long tmp = dvs;
+            long multiplier = 1;
+            while( dvd >= (tmp << 1)) {
+                tmp <<= 1;
+                multiplier <<=1;
+            }
+            System.out.println(tmp);
+            System.out.println(multiplier);
+            dvd -= tmp;
+            res += multiplier;
+            System.out.println(dvd);
+            System.out.println(res + "==");
+        }
+        
+        // while(dvd >= dvs) {
+        //     dvd -=dvs;
+        //     res++;
+        //     System.out.println(dvd);
+        //     System.out.println(res + "==");
+        // }
+        return sign == 1 ? (int) res : (int) -res;
+        
+    }
+}
